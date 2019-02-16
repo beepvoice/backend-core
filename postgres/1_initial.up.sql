c4cb9c35da09ba555a01acae1e10ee93c7b7805d
UnicodingUnicorn <7555ic@gmail.com>
2019-02-16 13:02:50 +0800

    IF NOT EXISTS

--- postgres/1_initial.up.sql
+++ postgres/1_initial.up.sql
@@ -1,25 +1,24 @@
 
-CREATE TABLE "user" (
+CREATE TABLE IF NOT EXISTS "user" (
 	id BYTEA PRIMARY KEY,
 	first_name VARCHAR(65535),
 	last_name VARCHAR(65535),
 	phone_number VARCHAR(32) UNIQUE
 );
 
-CREATE TABLE "conversation" (
+CREATE TABLE IF NOT EXISTS "conversation" (
 	id BYTEA PRIMARY KEY,
 	title VARCHAR(65535)
 );
 
-CREATE TABLE member (
+CREATE TABLE IF NOT EXISTS member (
 	"user" BYTEA REFERENCES "user"(id),
 	"conversation" BYTEA REFERENCES "conversation"(id),
 	UNIQUE ("user", "conversation")
 );
 
-CREATE TABLE contact (
+CREATE TABLE IF NOT EXISTS contact (
 	"user" BYTEA REFERENCES "user"(id),
 	contact BYTEA REFERENCES "user"(id),
 	UNIQUE ("user", contact)
 );
-
