117aefc0fcbbb35c19edb4968675b1dc9793be90
Ambrose Chua <ambrose@chua.family>
2019-08-04 13:19:07 +0800

    Split handlers in preparation for testing

--- types.go
+++ types.go
@@ -3,16 +3,20 @@ package main
 type Conversation struct {
 	ID      string `json:"id"`      // id
 	Title   string `json:"title"`   // title
-  DM      bool   `json:"dm"`      // dm
-  Picture string `json:"picture"` // picture
+	DM      bool   `json:"dm"`      // dm
+	Picture string `json:"picture"` // picture
 }
 
 type User struct {
 	ID          string `json:"id"`           // id
-	Username 		string `json:"username"`		 // username
-  Bio         string `json:"bio"`          // bio
-  ProfilePic  string `json:"profile_pic"`  // profile_pic
+	Username    string `json:"username"`     // username
+	Bio         string `json:"bio"`          // bio
+	ProfilePic  string `json:"profile_pic"`  // profile_pic
 	FirstName   string `json:"first_name"`   // first_name
 	LastName    string `json:"last_name"`    // last_name
 	PhoneNumber string `json:"phone_number"` // phone_number
+}
+
+type PhoneNumber struct {
+	PhoneNumber string `json:"phone_number"`
 }
